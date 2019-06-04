c8fc6e91b5aea5775b45e76df3f252927a464668
zhaowy <82626363@qq.com>
2019-06-05 00:02:29 +0800

    修改全局拦截信息

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ app.use(bodyParser.json());// 解析post提交的数据为json格式
 
 //设置跨域访问
 app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  console.log(req.hostname)
+  if(req.method==='POST'){
+    console.log('POST请求参数body', req.body)// application/x-www-form-urlencoded时
+  }
+  if(req.method==='GET'){
+    console.log('GET请求参数query', req.query)// 
+  }
+  // 服务器允许被那些origin访问。，*或者www.example.com
+  // res.header("Access-Control-Allow-Origin", "*") 
+  // 预设支持的方法
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
+  // 预检将在正式请求的headers字段中出现的首部信息，以下这些特定的首部是一直允许的：Accept, Accept-Language, Content-Language, Content-Type
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -40,12 +51,12 @@ app.get("/", function(req, res) {
 
 app.get("/test", function(req, res) {
   var query = req.query
-  console.log('本次get请求参数', query)
+  // console.log('本次get请求参数', query)
   res.send({data: {params: query},emessage:"request success",statusCode:200});
 });
 
 app.post("/test",function(req, res){
   var body = req.body // post请求参数为json时
-  console.log('本次post请求参数', body)
+  // console.log('本次post请求参数', body)
   res.send({data: {params: body},emessage:"request success",statusCode:200});
 })
